cda3faabd53eca7cb8b2f431d09bd274a0b12dcd
luizmigueldev <luizmigueln.dev@gmail.com>
2021-08-03 14:56:37 -0300

    Utilizizando useStates e useEffect

--- src/App.js
+++ src/App.js
@@ -3,5 +3,23 @@ import React, { useState, useEffect } from "react";
 
 
 export default function App() {
-  return ();
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(async () => {
+    const response = await fetch('https://api.github.com/users/luizmigueldev/repos');
+    const data = await response.json();
+
+    setRepositories(data);
+  }, [])
+
+  return (
+    <ul>
+      {
+        repositories.map(repo => (
+          <li key={repo.id} >{repo.name}</li>
+        ))
+      }
+    </ul>
+  );
 }
